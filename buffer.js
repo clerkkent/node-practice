65d2aa8058df36f71c3759932716a5eb86b7aedf
lichenglong06 <lichenglong06@baidu.com>
2017-10-31 20:45:57 +0800

    i

---
+++ buffer.js
@@ -0,0 +1,26 @@
+buf = new Buffer(26);
+for (var i = 0 ; i < 26 ; i++) {
+  buf[i] = i + 97;
+}
+
+console.log( buf.toString('ascii'));       // 输出: abcdefghijklmnopqrstuvwxyz
+console.log( buf.toString('ascii',0,5));   // 输出: abcde
+console.log( buf.toString('utf8',0,5));    // 输出: abcde
+console.log( buf.toString(undefined,0,5)); // 使用 'utf8' 编码, 并输出: abcde
+console.log(buf.toString('utf8'))
+
+var json = buf.toJSON(buf)
+console.log(json)
+
+
+var buffer1 = new Buffer('ABC');
+var buffer2 = new Buffer('ABCD');
+var result = buffer1.compare(buffer2);
+console.log(result)
+if(result < 0) {
+   console.log(buffer1 + " 在 " + buffer2 + "之前");
+}else if(result == 0){
+   console.log(buffer1 + " 与 " + buffer2 + "相同");
+}else {
+   console.log(buffer1 + " 在 " + buffer2 + "之后");
+}
---
+++ yield.js
@@ -0,0 +1,16 @@
+function* numbers() {
+    console.log('function start.');
+
+    var v1 = yield 0;
+    console.log('v1 = ' + v1);
+
+    var v2 = yield 1;
+    console.log('v2 = ' + v2);
+
+    return 5;
+}
+
+var nums = numbers();
+console.log(nums.next(2))
+console.log(nums.next(3))
+console.log(nums.next(4))
